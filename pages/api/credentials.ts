d55e7455c0d1de53fa312c50767154d6d27681f5
jonahlipsky <jonahlipsky@gmail.com>
2022-12-14 21:50:07 -0500

    add validation

--- pages/api/credentials.ts
+++ pages/api/credentials.ts
@@ -1,5 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import prisma from "../../lib/prismadb";
+import { ListBucketsCommand, S3Client } from "@aws-sdk/client-s3";
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   // res.status(200).json({ name: "John Doe" });
@@ -16,6 +17,43 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
         id: session?.userId,
       },
     });
+
+    const { accessKey, secretAccessKey } = req.body;
+
+    // validation -- i want to verify here that there's a bucket called attic (or attic-dev for dev)
+    const customerS3Client: S3Client = new S3Client({
+      region: "us-east-2",
+      credentials: {
+        accessKeyId: accessKey,
+        secretAccessKey: secretAccessKey,
+      },
+    });
+
+    // instead of list buckets i could probably do a search for the exact bucket i want
+    const command = new ListBucketsCommand({});
+    try {
+      const buckets = await customerS3Client.send(command);
+      let bucketFound = false;
+      let targetBucket = "attic";
+      buckets.Buckets?.forEach((bucket) => {
+        if (process.env.NODE_ENV !== "production") {
+          targetBucket = "attic-dev";
+        }
+
+        if (bucket.Name == targetBucket) {
+          bucketFound = true;
+        }
+      });
+      if (bucketFound == false) {
+        console.log("bucket not found");
+        res.end;
+      }
+    } catch {
+      console.log("credentials incorrect");
+      res.end;
+    }
+
+    // key update
 
     res.end;
   }
